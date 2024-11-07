412a34d6a392c3adf510ef753755027e7ddf6fba
Son Artem <n410tsacc@gmail.com>
2024-11-07 22:16:27 +0700

    update files

--- app/config.py
+++ app/config.py
@@ -6,11 +6,11 @@ class Config(object):
     UPLOAD_PATH = '/static/upload'
     SERVER_PATH = ROOT + UPLOAD_PATH
 
-    USER = os.environ.get('POSTGRES_USER', 'tyoma')
-    PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'tyoma')
-    HOST = os.environ.get('POSTGRES_HOST', '127.0.0.1')
-    PORT = os.environ.get('POSTGRES_PORT', '5432')
-    DB = os.environ.get('POSTGRES_DB', 'mydb')
+    USER = os.environ.get('POSTGRES_USER')
+    PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+    HOST = os.environ.get('POSTGRES_HOST')
+    PORT = os.environ.get('POSTGRES_PORT')
+    DB = os.environ.get('POSTGRES_DB')
 
     SQLALCHEMY_DATABASE_URI = f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}'
     SECRET_KEY = 'asjdfasjkf34itu3g4793f97#&&$292341'
